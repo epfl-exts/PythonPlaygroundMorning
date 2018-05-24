3dbff5e379a6897245d1fed27188cea5b725a48b
Sue Cheatham <sueBrite@googlemail.com>
2018-05-24 13:55:51 +0200

    Add files to get started

---
+++ Graphics/myFirstPython.py
@@ -0,0 +1,17 @@
+# First steps with Python
+# Run the program, change it and run again
+
+print('hello')
+print(1+1)
+
+x = 2          
+y = 3.2       
+z = 'hello'
+# print(x+y)
+
+
+number_icecreams = 2
+greeting = 'hello'
+#print(greeting, "I would like ", number_icecreams, "icecreams")
+
+
---
+++ Graphics/turtleGraphics.py
@@ -0,0 +1,10 @@
+# Turtle Graphics
+
+import turtle                               
+myScreen = turtle.Screen()        
+myTurtle = turtle.Turtle()      
+myTurtle.forward(100)         
+myTurtle.right(90)                  
+myTurtle.forward(100)   
+
+myScreen.exitonclick()  
